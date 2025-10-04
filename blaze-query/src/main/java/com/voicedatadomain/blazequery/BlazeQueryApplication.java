a513c45a5a5b0284afba9fb0726f66d40b27938e
NotJustCatCoder <1357329777@qq.com>
2025-10-04 15:36:27 +0800

    更新目录结构

--- blaze-query/src/main/java/com/voicedatadomain/blazequery/BlazeQueryApplication.java
+++ blaze-query/src/main/java/com/voicedatadomain/blazequery/BlazeQueryApplication.java
@@ -1,4 +1,4 @@
-package com.springai.blazequery;
+package com.voicedatadomain.blazequery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- blaze-query/src/test/java/com/voicedatadomain/blazequery/BlazeQueryApplicationTests.java
+++ blaze-query/src/test/java/com/voicedatadomain/blazequery/BlazeQueryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springai.blazequery;
+package com.voicedatadomain.blazequery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
