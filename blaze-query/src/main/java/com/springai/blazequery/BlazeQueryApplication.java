8f3755517332875343ad5dc1e62ad48b085ac063
NotJustCatCoder <1357329777@qq.com>
2025-10-04 15:18:23 +0800

    项目初步创建+DDD架构设计

---
+++ blaze-query/src/main/java/com/springai/blazequery/BlazeQueryApplication.java
@@ -0,0 +1,13 @@
+package com.springai.blazequery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlazeQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlazeQueryApplication.class, args);
+    }
+
+}
